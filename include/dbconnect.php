ce1390f41feec1d6dc08bdd378fbbadd331d9f9c
John Dovey <dovey.john@gmail.com>
2021-02-14 00:48:25 -0500

    db

--- include/dbconnect.php
+++ include/dbconnect.php
@@ -5,21 +5,3 @@ $link=mysqli_connect("127.0.0.1", "justdo","shiya30") or
 mysqli_select_db("story_db") or
 	die ("Could not select database");
 ?>
-<?php
-// Establish Database Connection
-
-class DB extends SQLite3
-{
-        function __construct( $file )
-        {
-            $this->open( $file );
-        }
-}
-
-$link = new DB('./myStory.db');
-if(!$link){
-    echo "<h1>" . $links->lastErrorMsg() . "</h1>";
- } else {
-    // echo "<h1>Opened database successfully</h1>\n";
- }
-?>
